fd51742edd8f76dc2b35bb3d8515f6b53502ab32
h3nnn4n <uber.renan@gmail.com>
2016-05-30 15:16:59 -0300

    Simple GUI for testing

---
+++ pitao/mainWindow.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainWindow.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.frame = QtGui.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 639, 437))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.frame_2 = QtGui.QFrame(self.frame)
+        self.frame_2.setGeometry(QtCore.QRect(10, 10, 141, 421))
+        self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame_2.setObjectName(_fromUtf8("frame_2"))
+        self.groupBox = QtGui.QGroupBox(self.frame_2)
+        self.groupBox.setGeometry(QtCore.QRect(10, 0, 120, 121))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.pushButton = QtGui.QPushButton(self.groupBox)
+        self.pushButton.setGeometry(QtCore.QRect(10, 30, 101, 23))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(self.groupBox)
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 60, 101, 23))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.pushButton_3 = QtGui.QPushButton(self.groupBox)
+        self.pushButton_3.setGeometry(QtCore.QRect(10, 90, 101, 23))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.mapView = QtGui.QFrame(self.frame)
+        self.mapView.setGeometry(QtCore.QRect(160, 10, 471, 421))
+        self.mapView.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.mapView.setFrameShadow(QtGui.QFrame.Raised)
+        self.mapView.setObjectName(_fromUtf8("mapView"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 20))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuArquivo = QtGui.QMenu(self.menubar)
+        self.menuArquivo.setObjectName(_fromUtf8("menuArquivo"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionSair = QtGui.QAction(MainWindow)
+        self.actionSair.setObjectName(_fromUtf8("actionSair"))
+        self.menuArquivo.addAction(self.actionSair)
+        self.menubar.addAction(self.menuArquivo.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.groupBox.setTitle(_translate("MainWindow", "Botões", None))
+        self.pushButton.setText(_translate("MainWindow", "Botão 1", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Botão 2", None))
+        self.pushButton_3.setText(_translate("MainWindow", "Botão 2", None))
+        self.menuArquivo.setTitle(_translate("MainWindow", "Arquivo", None))
+        self.actionSair.setText(_translate("MainWindow", "Sair", None))
+
